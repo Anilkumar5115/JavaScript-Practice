313f9cae1ab68acbef435a060101877e42d2f98f
Anilkumar5115 <anilkumarvaddepally5@gmail.com>
2023-10-30 01:43:30 -0400

    This is my First Commit

---
+++ addition.js
@@ -0,0 +1,8 @@
+
+//called function(preparation function)
+function addition(n1, n2){
+
+    var result;
+    result = n1 +n2;
+    Document.write(result)
+}
---
+++ addvalidation.js
@@ -0,0 +1,8 @@
+//without Validations
+function doaddition(){
+var n1 = document.getElementById("id1").Value
+var n2 = document.getElementById("id2").value
+
+    var total = eval(n1) + eval +(n2)
+    document.getElementById("id3").value = total
+}
